81fdd53a34af81fe858f024bb008c0cd8d4e2d87
Yutaro Tsuji <ytsuji91@gmail.com>
2019-01-14 04:59:31 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,8 @@ def roll_call_dwarves(dwarfs)
 end
 
 def summon_captain_planet(planeteer_calls)
-  
+  planeteer_calls.collect do |calls|
+    calls << "!"
 end
 
 def long_planeteer_calls# code an argument here
