7e1249da595e42f1220f9475eedc60e1fff19e0f
Yutaro Tsuji <ytsuji91@gmail.com>
2019-01-14 05:03:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,5 +19,6 @@ end
 def find_the_cheese(array_of_strings)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array_of_strings.find do 
+  array_of_strings.find do |cheese|
+    
 end
