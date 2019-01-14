576233c75619988e905631a14e58e502024a4aac
Yutaro Tsuji <ytsuji91@gmail.com>
2019-01-14 05:03:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,8 @@ def long_planeteer_calls(array_of_calls)
   end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array_of_strings)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
 end
