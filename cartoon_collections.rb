e264d3338478d8a83c68c5a095b886039419c299
Yutaro Tsuji <ytsuji91@gmail.com>
2019-01-14 05:01:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,8 +10,8 @@ def summon_captain_planet(planeteer_calls)
   end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array_of_calls)
+  
 end
 
 def find_the_cheese# code an argument here
